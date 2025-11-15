dd80934bb242d6be1690d0d875ebb3f12e8fe1b6
AMIT KUMAR SINGH <92827711+amitksingh2103@users.noreply.github.com>
2025-11-15 15:42:39 +0530

    Create streamlit_app.py

---
+++ src/streamlit_app.py
@@ -0,0 +1,10 @@
+import streamlit as st, requests
+
+st.title("🧠 RAG with PDF + Message History (Lambda Backend)")
+
+api_url = "https://0aft1y1h6a.execute-api.us-east-1.amazonaws.com/default/RagPDFBackend"
+
+query = st.text_input("💬 Ask your question:")
+if st.button("Submit"):
+    response = requests.post(api_url, json={"query": query})
+    st.write(response.json()["answer"])
